2aa97376a54e45898ea52c1cc4684484d84576f0
LALIT KUMAR JANGID <96244930+lalitkumarjangid@users.noreply.github.com>
2024-03-06 23:26:17 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,60 +1,65 @@
-// backend/db.js
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-// Attempt to connect to the MongoDB database
-mongoose.connect(MONGO_URL//use your mongoDb URL here)
+const mongoUrl = process.env.VITE_MONGO_URL;
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error.message);
   });
+
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        minLength: 3,
-        maxLength: 30
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 6
-    },
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50
+  }
 });
 
-
 const accountSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    balance: {
-        type: Number,
-        required: true
-    },
-})
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  balance: {
+    type: Number,
+    required: true
+  },
+});
+
 const Account = mongoose.model('Account', accountSchema);
 const User = mongoose.model('User', userSchema);
 
 module.exports = {
-	User,
-    Account
+  User,
+  Account
 };
